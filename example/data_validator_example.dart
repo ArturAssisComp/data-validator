6cfe828dc0a874e7a9c24342b3c5a6a6f2a408d0
Artur Assis Alves <arturassiscomp@gmail.com>
2024-01-16 10:45:47 -0300

    update validator example

--- example/data_validator_example.dart
+++ example/data_validator_example.dart
@@ -3,6 +3,12 @@ import 'dart:developer';
 import 'package:data_validator/data_validator.dart';
 
 void main() {
-  final overallStatus = ValidationStatus(validationName: 'this status');
+  final overallStatus = ValidationStatus(validationName: 'this status')
+    ..addResult(
+      const UnitValidationStatus(
+        nodeName: 'any',
+        status: UnitValidationStatusCode.notDefined,
+      ),
+    );
   log(overallStatus.status.toString());
 }
